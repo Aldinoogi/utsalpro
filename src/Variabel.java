7a0acfebf2fec0f701e1878a9729d7210c6d5610
Aldinoogi <93021737+Aldinoogi@users.noreply.github.com>
2021-11-20 10:32:52 +0700

    utsalpro

---
+++ src/Variabel.java
@@ -0,0 +1,11 @@
+public class Variabel {
+
+	public static void main(String args[])
+	{
+		String Nama = "Aldino Ogi";
+		int Tahun = 1995;
+		System.out.println("Assalamu'alaikum");
+		System.out.printf("Nama Saya : %s", Nama);
+		System.out.printf("\nSaya Lahir di Tahun : %d", Tahun);
+	}
+}
